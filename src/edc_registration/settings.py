c7e108fd350e6e2b78a5f6c5a4c81d7d64c3e2d9
erikvw <ew2789@gmail.com>
2016-08-24 16:05:38 +0200

    move mixins to models_mixin, remove methods for enrollment cap, see edc_protocol

--- src/edc_registration/settings.py
+++ src/edc_registration/settings.py
@@ -44,7 +44,9 @@ INSTALLED_APPS = [
     'django_crypto_fields.apps.AppConfig',
     'django_revision.apps.AppConfig',
     'edc_base.apps.AppConfig',
+    'edc_protocol.apps.AppConfig',
     'edc_registration.apps.AppConfig',
+    'example.apps.AppConfig',
 ]
 
 MIDDLEWARE_CLASSES = (
