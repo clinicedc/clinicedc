e26d49f6ad88bb28dac0c3eed6f80da60ec6b249
erikvw <ew2789@gmail.com>
2026-02-09 19:07:10 -0600

    fix datetime_in_window to handle extended upper windows

--- src/edc_visit_schedule/schedule/window.py
+++ src/edc_visit_schedule/schedule/window.py
@@ -1,6 +1,7 @@
 from dateutil.relativedelta import relativedelta
 from django.conf import settings
 from django.utils.translation import gettext as _
+
 from edc_utils import (
     ceil_secs,
     convert_php_dateformat,
@@ -11,44 +12,46 @@ from edc_utils import (
 
 from ..exceptions import (
     ScheduledVisitWindowError,
-    ScheduleError,
     UnScheduledVisitWindowError,
 )
 from ..utils import get_enforce_window_period_enabled
-from .visit_collection import VisitCollectionError
 
 
 class Window:
     def __init__(
         self,
         name=None,
-        visits=None,
         dt=None,
-        timepoint_datetime=None,
-        visit_code=None,
-        visit_code_sequence=None,
         baseline_timepoint_datetime=None,
+        timepoint_datetime=None,
+        visit=None,
+        next_visit=None,
+        visit_code_sequence=None,
     ):
         self.name = name
-        self.visits = visits
-        self.visit_code = visit_code
-        self.visit_code_sequence = visit_code_sequence
 
         # convert dates to local tzinfo
-        self.timepoint_datetime = to_local(timepoint_datetime)
         self.dt = to_local(dt)
+        self.timepoint_datetime = to_local(timepoint_datetime)
         self.baseline_timepoint_datetime = to_local(baseline_timepoint_datetime)
+
+        self.visit = visit
+        self.visit.timepoint_datetime = self.timepoint_datetime
+
+        self.next_visit = next_visit
+        if self.next_visit:
+            self.next_visit.timepoint_datetime = (
+                self.baseline_timepoint_datetime + self.next_visit.rbase
+            )
+
+        self.visit_code_sequence = visit_code_sequence
 
     @property
     def datetime_in_window(self):
         if get_enforce_window_period_enabled():
             if not self.dt:
                 raise UnScheduledVisitWindowError("Invalid datetime")
-            try:
-                self.visits.get(self.visit_code)
-            except VisitCollectionError as e:
-                raise ScheduleError(e) from e
-            if self.is_scheduled_visit or not self.visits.next(self.visit_code):
+            if self.is_scheduled_visit:
                 self.raise_for_scheduled_not_in_window()
             else:
                 self.raise_for_unscheduled_not_in_window()
@@ -60,12 +63,10 @@ class Window:
 
     def get_window_gap_days(self) -> int:
         days = 0
-        visit = self.visits.get(self.visit_code)
-        next_visit = self.visits.next(self.visit_code)
-        if visit.add_window_gap_to_lower and next_visit:
+        if self.visit.add_window_gap_to_lower and self.next_visit:
             days = abs(
-                (self.timepoint_datetime + visit.rupper)
-                - (self.timepoint_datetime - next_visit.rlower)
+                (self.timepoint_datetime + self.visit.rupper)
+                - (self.timepoint_datetime - self.next_visit.rlower)
             ).days
         return days
 
@@ -77,11 +78,10 @@ class Window:
         In this case, `visit` is the object from schedule and
         not a model instance.
         """
-        visit = self.visits.get(self.visit_code)
-        visit.timepoint_datetime = self.timepoint_datetime
+
         gap_days = self.get_window_gap_days()
-        lower = floor_secs(to_local(visit.dates.lower) - relativedelta(days=gap_days))
-        upper = ceil_secs(to_local(visit.dates.upper))
+        lower = floor_secs(to_local(self.visit.dates.lower) - relativedelta(days=gap_days))
+        upper = ceil_secs(to_local(self.visit.dates.upper))
         if not (lower <= floor_secs(to_local(self.dt)) <= upper):
             lower_date = to_local(lower).strftime(
                 convert_php_dateformat(settings.SHORT_DATETIME_FORMAT)
@@ -97,7 +97,7 @@ class Window:
                 "Invalid. Date falls outside of the "
                 f"window period for this `scheduled` visit. "
                 f"Expected a date between {lower_date} "
-                f"and {upper_date} for {self.visit_code}. Got `{dt}`."
+                f"and {upper_date} for {self.visit.code}. Got `{dt}`."
             )
 
     def raise_for_unscheduled_not_in_window(self):
@@ -112,29 +112,47 @@ class Window:
         In this case, `visit` is the object from schedule and
         not a model instance.
         """
-        next_visit = self.visits.next(self.visit_code)
-        next_timepoint_datetime = self.visits.timepoint_dates(
-            dt=self.baseline_timepoint_datetime
-        ).get(next_visit)
-        if not (
-            floor_secs(to_local(self.dt))
-            < floor_secs(to_local(next_timepoint_datetime - next_visit.rlower))
-        ):
-            dt_lower = formatted_date(next_timepoint_datetime - next_visit.rlower)
-            dt = formatted_date(self.dt)
-            raise UnScheduledVisitWindowError(
-                _(
-                    "Invalid datetime. Falls outside of the "
-                    "window period for this `unscheduled` visit. "
-                    "Expected a datetime before the next visit. "
-                    "Next visit is `%(next_visit_code)s` expected any time "
-                    "from `%(dt_lower)s`."
-                    "Got `%(visit_code)s`@`%(dt)s`. "
-                )
-                % dict(
-                    next_visit_code=next_visit.code,
-                    dt_lower=dt_lower,
-                    visit_code=self.visit_code,
-                    dt=dt,
+        formatted_dt = formatted_date(self.dt)
+        if self.next_visit:
+            in_window = floor_secs(to_local(self.dt)) < floor_secs(
+                to_local(self.next_visit.timepoint_datetime - self.next_visit.rlower)
+            )
+            msg = _(
+                "Invalid datetime. Falls outside of the "
+                "window period for this `unscheduled` visit. "
+                "Expected a datetime before the next visit. "
+                "Next visit is `%(next_visit_code)s` expected any time "
+                "from `%(dt_lower)s`."
+                "Got `%(visit_code)s`@`%(dt)s`. "
+            ) % dict(
+                next_visit_code=self.next_visit.code,
+                dt_lower=formatted_date(
+                    self.next_visit.timepoint_datetime - self.next_visit.rlower
+                ),
+                visit_code=self.visit.code,
+                dt=formatted_dt,
+            )
+        else:
+            in_window = floor_secs(to_local(self.dt)) < floor_secs(
+                to_local(
+                    self.visit.timepoint_datetime
+                    + (self.visit.rupper_extended or self.visit.rupper)
                 )
             )
+            msg = _(
+                "Invalid datetime. Falls outside of the "
+                "window period for this `unscheduled` visit. "
+                "Expected a datetime before `%(dt_upper)s`."
+                "Got `%(visit_code)s`@`%(dt)s`. "
+            ) % dict(
+                dt_upper=formatted_date(
+                    to_local(
+                        self.visit.timepoint_datetime
+                        + (self.visit.rupper_extended or self.visit.rupper)
+                    )
+                ),
+                visit_code=self.visit.code,
+                dt=formatted_dt,
+            )
+        if not in_window:
+            raise UnScheduledVisitWindowError(msg)
